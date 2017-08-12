48e3596f1b5560217d5907686921862306712ecf
snowyjone <w007878@hotmail.com>
2017-08-12 16:20:43 +0800

    new version

---
+++ main.py
@@ -0,0 +1,63 @@
+import numpy as np
+import tensorflow as tf
+import load_data as ldata
+import model
+import os
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "3"
+BATCH_SIZE = 128
+EPOCH_SIZE = 100
+
+def next_batch(x, y, batch_size=BATCH_SIZE):
+    i = 0
+    while(i < len(x)):
+        yield x[i:i + batch_size], y[i:i + batch_size]
+        i = i + batch_size
+
+if __name__ == '__main__':
+
+    sess = tf.Session()
+    gan = model.GAN()
+    
+    images, labels = ldata.load_SVHN()
+    images = (images - 0.5) * 2.
+    
+    label_ = tf.placeholder(tf.float32, [None, 2])
+    
+    dis_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=label_, logits=gan.dis))
+    gen_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=label_, logits=gan.dis_gen))
+
+    gen_train_step = tf.train.MomentumOptimizer(0.0002, 0.5).minimize(gen_loss)
+    dis_train_step = tf.train.MomentumOptimizer(0.0002, 0.5).minimize(dis_loss)
+
+    print dis_train_step
+    
+    print gen_train_step
+    sess.run(tf.global_variables_initializer())
+
+    for step in range(EPOCH_SIZE):
+
+        batch_step = 0
+        for x, _ in next_batch(images, labels):
+            batch_step = batch_step + 1
+
+            input_noise = model.random_init([BATCH_SIZE, 100])
+            xn = gan.gen.eval(session=sess, feed_dict={gan.raw_input_noise:input_noise})
+            yn = np.array([[0, 1]] * len(x))
+            
+            x_ = tf.reshape(x, [len(x), 32 * 32 * 3])
+            y = np.array([[1, 0]] * BATCH_SIZE)
+
+            data = np.concatenate((x_, xn))
+            label = np.concatenate((y, yn))
+
+            sess.run(dis_train_step, feed_dict={gan.raw_input_image:data, label_:label})
+
+            if batch_step % 20 == 0:
+                print("Epoch %d, Batch: %d" % (step, batch_step))
+                input_noise = model.random_init([BATCH_SIZE, 100])
+                y = np.array([[1, 0]] * BATCH_SIZE)
+                sess.run(gen_train_step, feed_dict={gan.raw_input_noise:input_noise, label_:y})
+
+        data = gan.gen.eval(session=sess, feed_dict={gan.raw_input_noise:model.random_init([100, 100])})
+        ldata.cv2_save(n=10, m=10, data=(data + 1.) / 2., file_path="gen/{}.png".format(step))
